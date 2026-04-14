e7c756054dae14452d57c0b06a567434435ce8f8
brockii <89240640+brockii@users.noreply.github.com>
2026-04-14 19:54:06 +0530

    Add: three-integer-sum - submission-8

---
+++ Data Structures & Algorithms/three-integer-sum/submission-8.py
@@ -0,0 +1,30 @@
+class Solution:
+    def threeSum(self, nums: list[int]) -> list[list[int]]:
+        nums = sorted(nums)
+        nums_set = set(nums)
+        zero_indices = []
+        if nums[0] > 0 or nums[-1] < 0:
+            return zero_indices
+
+        for i in range(len(nums) - 2):
+            j = i + 1
+            if nums[i] + nums[j] > 0 or i > 0 and nums[i - 1] == nums[i]: continue
+
+            while j < len(nums) - 1:
+                if j>i+1 and nums[j] == nums[j-1]:
+                    j += 1
+                    continue
+                if -1 * (nums[i] + nums[j]) not in nums_set:
+                    j += 1
+                    continue
+                k = j + 1
+                while k < len(nums):
+                    if k>j+1 and nums[k] == nums[k-1]:
+                        k += 1
+                        continue
+                    if nums[i] + nums[j] + nums[k] == 0:
+                        zero_indices.append([nums[i], nums[j], nums[k]])
+                    k += 1
+                j += 1
+
+        return zero_indices
